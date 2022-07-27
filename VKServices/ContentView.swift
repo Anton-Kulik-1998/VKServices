58614208fe37f656786357df4079e6d0915efde4
Антон Кулик <anton.smile.1998@gmail.com>
2022-07-27 23:52:32 +0300

    Initial Commit

---
+++ VKServices/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  VKServices
+//
+//  Created by Антон Кулик on 25.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ VKServices/VKServicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKServicesApp.swift
+//  VKServices
+//
+//  Created by Антон Кулик on 25.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKServicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
